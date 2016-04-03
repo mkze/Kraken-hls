cd96e9805f7e45b704ec99a5cd3dc8881d4bced0
mkze <mkzenicholson@gmail.com>
2016-04-02 20:39:04 -0400

    update gulp tasks
    - group files in arrays, remove forEach loop

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,39 @@
-//gulpfile.js
-//task runner
-//v0.0.0
-//
 
-(() => {
+"use strict";
 
-    "use strict";
+const gulp = require('gulp');
 
-    const gulp = require('gulp');
+let js_libs = [
+    'node_modules/angular/angular.min.js',
+    'node_modules/angular-material/angular-material.min.js',
+    'node_modules/angular-animate/angular-animate.min.js',
+    'node_modules/angular-aria/angular-aria.min.js',
+    'node_modules/hls.js/dist/hls.min.js',
+    'node_modules/angular-new-router/dist/router.es5.min.js'
+];
 
-    let js_libs = [
-        'node_modules/angular/angular.min.js',
-        'node_modules/angular-material/angular-material.min.js',
-        'node_modules/angular-animate/angular-animate.min.js',
-        'node_modules/angular-aria/angular-aria.min.js',
-        'node_modules/hls.js/dist/hls.min.js',
-        'node_modules/angular-new-router/dist/router.es5.min.js'
-    ];
+let css_libs = [
+    'node_modules/angular-material/angular-material.min.css',
+    'node_modules/font-awesome/css/font-awesome.min.css'
+];
 
+let fonts = [
+    'node_modules/font-awesome/fonts/*'
+];
 
+gulp.task('move_libs', () => {
+    gulp.src(js_libs)
+        .pipe(gulp.dest('kraken/lib/js/'));
+});
 
-    gulp.task('move_libs', () => {
-        js_libs.forEach((lib) => {
-            gulp.src(lib)
-                .pipe(gulp.dest('kraken/lib/js/'));
-        });
-    });
+gulp.task('move_css', () => {
 
-    gulp.task('move_css', () => {
-
-        //main angular-material css
-        gulp.src('node_modules/angular-material/angular-material.min.css')
-            .pipe(gulp.dest('kraken/lib/css/'));
-
-        //main font-awesome css
-        gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-            .pipe(gulp.dest('kraken/lib/css/'));
+    gulp.src(css_libs)
+        .pipe(gulp.dest('kraken/lib/css/'));
         
-        //font-awesome font files
-        gulp.src('node_modules/font-awesome/fonts/*')
-            .pipe(gulp.dest('kraken/lib/fonts/'));
+    gulp.src(fonts)
+        .pipe(gulp.dest('kraken/lib/fonts/'));
         
-    });
+});
 
-    gulp.task('default', ['move_libs', 'move_css']);
-
-})();
+gulp.task('default', ['move_libs', 'move_css']);
